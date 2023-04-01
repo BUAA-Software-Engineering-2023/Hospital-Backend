4b5016052766e527cf96497c649cbb9ea1c78971
BienBoy <285915362@qq.com>
2023-04-01 22:11:33 +0800

    调整Django项目

--- HospitalBackend/wsgi.py
+++ HospitalBackend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Hospital-Backend project.
+WSGI config for HospitalBackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hospital-Backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HospitalBackend.settings')
 
 application = get_wsgi_application()
