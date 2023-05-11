f19ead2ef2943305cd632a3f92417b69c77f2856
pesenteur <931373882@qq.com>
2023-05-11 20:01:25 +0800

    创建django项目

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HospitalBackend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hospital_Backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
