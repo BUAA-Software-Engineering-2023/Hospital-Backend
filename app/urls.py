9baf38093c1ea6dcba41efe739948beefa04519a
pesenteur <931373882@qq.com>
2023-05-12 23:05:19 +0800

    添加VIews
    VacancyList
    NewsList

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import DepartmentList, DoctorList,NotificationList,CarouselMapList
+from .views import DepartmentList, DoctorList,NotificationList,CarouselMapList,NewsList,VacancyList
 
 urlpatterns = [
     path('api/departmentList', DepartmentList.as_view()),
     path('api/doctorList', DoctorList.as_view()),
     path('api/notificationList', NotificationList.as_view()),
     path('api/carouselList',CarouselMapList.as_view()),
-    # path('api/newsList'),
+    path('api/newsList',NewsList.as_view()),
     # path('api/doctorDetail'),
-    # path('api/vacancyList')
+    path('api/vacancyList',VacancyList.as_view())
 ]
