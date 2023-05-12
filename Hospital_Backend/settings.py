73afdd0c3ff84a3f879122f59a86881617aa32dc
pesenteur <931373882@qq.com>
2023-05-12 19:07:37 +0800

    增加了一个api
    DepartmentList

--- Hospital_Backend/settings.py
+++ Hospital_Backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Hospital_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hospital',
+        'USER': 'root',
+        'PASSWORD':'Wuxinyu67981998',
+        'HOST':'',
+        'PORT':'3306'
     }
 }
 
