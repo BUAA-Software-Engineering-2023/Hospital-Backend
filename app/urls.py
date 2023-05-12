f5dc5453f05cc4f351003708b0af938213068587
Thebearcoding <1193617419@qq.com>
2023-05-12 21:36:03 +0800

    更新urls

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from .views import DepartmentList, DoctorList, NotificationList
 urlpatterns = [
     path('api/departmentList', DepartmentList.as_view()),
     path('api/doctorList', DoctorList.as_view()),
-    path('api/notificationList',NotificationList.as_view()),
+    path('api/notificationList', NotificationList.as_view()),
     # path('api/carouselList'),
     # path('api/newsList'),
     # path('api/doctorDetail'),
