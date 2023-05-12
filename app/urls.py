d3e2dd0fe17e7bdc9e9bb105d690c9ed1068b80d
Thebearcoding <1193617419@qq.com>
2023-05-12 23:04:55 +0800

    更新视图
    DoctorDetail

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import DepartmentList, DoctorList,NotificationList,CarouselMapList
+from .views import DepartmentList, DoctorList, NotificationList, CarouselMapList, DoctorDetail
 
 urlpatterns = [
     path('api/departmentList', DepartmentList.as_view()),
     path('api/doctorList', DoctorList.as_view()),
     path('api/notificationList', NotificationList.as_view()),
-    path('api/carouselList',CarouselMapList.as_view()),
+    path('api/carouselList', CarouselMapList.as_view()),
     # path('api/newsList'),
-    # path('api/doctorDetail'),
+    path('api/doctorDetail/<slug:doctor_id>', DoctorDetail.as_view()),
     # path('api/vacancyList')
 ]
