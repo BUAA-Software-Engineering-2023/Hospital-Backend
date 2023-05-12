8a264c552c362c9630a595e4d7fc71706f941b86
pesenteur <931373882@qq.com>
2023-05-12 21:33:11 +0800

    增加了Views
    CarouselMap

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import DepartmentList, DoctorList
+from .views import DepartmentList, DoctorList,NotificationList,CarouselMapList
 
 urlpatterns = [
     path('api/departmentList', DepartmentList.as_view()),
     path('api/doctorList', DoctorList.as_view()),
-    path('api/notificationList',),
-    path('api/carouselList'),
-    path('api/newsList'),
-    path('api/doctorDetail'),
-    path('api/vacancyList')
+    # path('api/notificationList',),
+    path('api/carouselList',CarouselMapList.as_view()),
+    # path('api/newsList'),
+    # path('api/doctorDetail'),
+    # path('api/vacancyList')
 ]
