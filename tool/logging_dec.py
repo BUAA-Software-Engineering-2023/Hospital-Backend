4960d4930af36cb1e7499cf23bebf0cde1fe2988
pesenteur <931373882@qq.com>
2023-05-14 00:00:35 +0800

    添加验证token的装饰器，以及注册（密码）的url

---
+++ tool/logging_dec.py
@@ -0,0 +1,22 @@
+import jwt
+from django.http import JsonResponse
+from django.conf import settings
+
+def logging_check(func):
+
+    def wrap(request,*args,**kwargs):
+
+        token = request.META.get('HTTP_AUTHORIZATION')
+        if token is not None:
+            try:
+                jwt_token = jwt.decode(token,settings.JWT_TOKEN_KEY)
+            except:
+                response ={
+                    "result":"0",
+                    "reason":"token error"
+                }
+                return JsonResponse(response)
+
+        return func(request,*args,**kwargs)
+
+    return wrap
