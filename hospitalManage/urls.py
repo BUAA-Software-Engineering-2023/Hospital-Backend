8055357764ea29a2ad34fe29f6c3273dbe3dacf4
Thebearcoding <1193617419@qq.com>
2023-05-14 18:58:32 +0800

    更新管理员放号管理于通知系统

--- hospitalManage/urls.py
+++ hospitalManage/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from .views import DepartmentManage, NotificationManage,VacancyManage
+urlpatterns = [
+    path('api/departmentManage', DepartmentManage.as_view()),
+    path('api/notificationManage',NotificationManage.as_view()),
+    path('api/vacancyManage',VacancyManage.as_view())
+]
