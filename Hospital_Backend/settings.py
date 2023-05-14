657d1b3758085bbd20e72ebba57e7105fd7e0405
Thebearcoding <1193617419@qq.com>
2023-05-14 14:06:39 +0800

    Update settings.py

--- Hospital_Backend/settings.py
+++ Hospital_Backend/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hospital',
         'USER': 'root',
-        'PASSWORD':'root',
+        'PASSWORD':'Athebear521989!',
         'HOST':'localhost',
         'PORT':'3306'
     }
