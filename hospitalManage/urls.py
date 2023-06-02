19309e8bf52fe5e204b4deeb341cea5b066ed36d
Thebearcoding <1193617419@qq.com>
2023-06-02 20:02:21 +0800

    更新

--- hospitalManage/urls.py
+++ hospitalManage/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import LoginView, DoctorManagement, ScheduleManage, DepartmentManage, NotificationManage, VacancyManage, \
-    LeaveListManage, ProcessLeave, vacancy_check, DoctorImage, VacancySettingManage, UploadImage, NewsManage,ChangeAdminPasswd,AdminIntroduction
+    LeaveListManage, ProcessLeave, vacancy_check, DoctorImage, VacancySettingManage, UploadImage, NewsManage,\
+    ChangeAdminPasswd,AdminIntroduction
 
 urlpatterns = [
     #
